0cce3d838cd9dc6a33afb9f1697600fed7f97556
Joshua Sung <yuniksung@gmail.com>
2021-02-10 00:52:33 -0600

    models-pizza and controller for pizza have created

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,68 @@
+const {Pizza} = require('../models');
+
+const pizzaController = {
+
+    // get All Pizza
+    getAllPizza(req, res) {
+        Pizza.found({
+
+        })
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err=> {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // get one pizza by id 
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({message: 'No pizza found with this id'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // Create pizza
+    createPizza({body}, res) {
+        Pizza.create(body)
+        .then(dbPizzaData=> res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err)); 
+    },
+
+    // update a pizza by id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({_id: params.id}, body, {new: true})
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({message: 'No pizza found with this id'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+        
+    },
+
+    // delete Pizza
+    deletePizza({params}, res) {
+        Pizza.findOneAndDelete({_id: params.id})
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({message:'No pizza found with this id'})
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.satus(400).json(err));
+    }
+};
+
+module.exports = pizzaController;

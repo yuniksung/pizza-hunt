4637cc8b53d5bcd100e24ba3016dd727dd5572f6
Joshua Sung <yuniksung@gmail.com>
2021-02-10 19:23:10 -0600

    comment function has added

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,19 +1,30 @@
 const { Pizza } = require('../models');
 
+
 const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
-
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -54,5 +65,7 @@ const pizzaController = {
       .catch(err => res.status(400).json(err));
   }
 };
+
+
 
 module.exports = pizzaController;
